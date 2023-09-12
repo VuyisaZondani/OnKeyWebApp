5cea059570a7b10f1ab2c37507c258a9a40792d3
VuyisaZondani <shasazondani@gmail.com>
2023-09-12 07:07:08 +0200

    .

--- OnKeyWebApp/Program.cs
+++ OnKeyWebApp/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using OnKeyWebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
