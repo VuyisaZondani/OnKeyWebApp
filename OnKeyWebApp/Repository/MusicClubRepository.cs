9fd90ecb4f4dbf923bdf6ed173342ca1d92e47ed
VuyisaZondani <shasazondani@gmail.com>
2023-09-12 07:05:13 +0200

    Added repository.

---
+++ OnKeyWebApp/Repository/MusicClubRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnKeyWebApp.Data;
+using OnKeyWebApp.Models;
+
+namespace OnKeyWebApp.Repository
+{
+    public class MusicClubRepository : IMusicClubRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MusicClubRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(MusicClub musicClub)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(MusicClub musicClub)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<MusicClub>> GetAllMusicClubs()
+        {
+            return await _context.MusicClubs.ToListAsync();
+        }
+
+        public Task<MusicClub> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(MusicClub musicClub)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
