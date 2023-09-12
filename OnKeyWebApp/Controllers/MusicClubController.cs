8892ebe3ca0428545dfc63b356afbbb7cc9433ba
VuyisaZondani <shasazondani@gmail.com>
2023-09-12 07:05:55 +0200

    Added controller.

---
+++ OnKeyWebApp/Controllers/MusicClubController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnKeyWebApp.Data;
+using OnKeyWebApp.Models;
+
+namespace OnKeyWebApp.Controllers
+{
+    public class MusicClubController : Controller
+    {
+        private readonly IMusicClubRepository _musicClubRepository;
+
+        public MusicClubController(IMusicClubRepository musicClubRepository )
+        {
+           _musicClubRepository = musicClubRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+           IEnumerable<MusicClub> musicClubs = await _musicClubRepository.GetAllMusicClubs();
+           return View(musicClubs);
+        }
+        public async Task<IActionResult> Detail(int Id)
+        {
+            return View();
+        }
+    }
+}
