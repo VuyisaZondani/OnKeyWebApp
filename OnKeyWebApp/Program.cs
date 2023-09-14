6819632c9870563564231a7af51d2b62930c1c56
VuyisaZondani <shasazondani@gmail.com>
2023-09-14 16:04:18 +0200

    .

--- OnKeyWebApp/Program.cs
+++ OnKeyWebApp/Program.cs
@@ -5,6 +5,7 @@ using OnKeyWebApp.Data.Interface;
 using OnKeyWebApp.Repository;
 using OnKeyWebApp.Service;
 using OnKeyWebApp.Data.Helpers;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +14,21 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IMusicClubRepository, MusicClubRepository>();
 builder.Services.AddScoped<IPhotoServices, PhotoService>();
 builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme);
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 var app = builder.Build();
-
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+{
+    //Seed SeedUsers and RolesAsync
+    
+    Seed.SeedData(app);
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -33,7 +43,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
