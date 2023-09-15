230e54f270a057e34201b212b7f518b9abb10282
VuyisaZondani <shasazondani@gmail.com>
2023-09-15 18:38:02 +0200

    Updated models.

--- OnKeyWebApp/Models/Event.cs
+++ OnKeyWebApp/Models/Event.cs
@@ -6,12 +6,12 @@ namespace OnKeyWebApp.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public string? Description { get; set; }
         public string? Location { get; set; }
         public string? ProfilePicUrl { get; set; }
         //public IFormFile Image { get; set; }
         //Relationship many to many
-        public List<MusicClub_Event> MusicClub_Events { get; set; }
+        //public List<MusicClub_Event> MusicClub_Events { get; set; }
     }
 }
--- OnKeyWebApp/Models/MusicClub.cs
+++ OnKeyWebApp/Models/MusicClub.cs
@@ -7,14 +7,14 @@ namespace OnKeyWebApp.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public Genre Genre { get; set; }
-        public string Street { get; set; }
-        public string Neighbourhood { get; set; }
-        public string ProfilePicUrl { get; set; }
+        public string? Street { get; set; }
+        public string? Neighbourhood { get; set; }
+        public string? ProfilePicUrl { get; set; }
         //public IFormFile Image { get; set; }
-        public List<MusicClub_Event> MusicClub_Events { get; set; }
+        //public List<MusicClub_Event> MusicClub_Events { get; set; }
     }
-        
+
 }
