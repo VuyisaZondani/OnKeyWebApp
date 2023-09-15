034a9763ae0d0c56bde84432f434f3f583941e26
VuyisaZondani <shasazondani@gmail.com>
2023-09-15 18:38:59 +0200

    Updated view model.

--- OnKeyWebApp/ViewModel/CreateMusicClubViewModel.cs
+++ OnKeyWebApp/ViewModel/CreateMusicClubViewModel.cs
@@ -5,13 +5,13 @@ namespace OnKeyWebApp.ViewModel
     public class CreateMusicClubViewModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public Genre Genre { get; set; }
-        public string Street { get; set; }
-        public string Neighbourhood { get; set; }
-        public string ProfilePicUrl { get; set; }
+        public string? Street { get; set; }
+        public string? Neighbourhood { get; set; }
+        public string? ProfilePicUrl { get; set; }
         public IFormFile Image { get; set; }
-        public string AppUserId { get; set; }
+        public string? AppUserId { get; set; }
     }
 }
