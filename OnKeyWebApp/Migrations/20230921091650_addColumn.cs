985c01ab618803974fe397b6a76b3ebdc7e7978b
VuyisaZondani <shasazondani@gmail.com>
2023-09-21 12:11:55 +0200

    Added and updated migrations.

---
+++ OnKeyWebApp/Migrations/20230921091650_addColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnKeyWebApp.Migrations
+{
+    public partial class addColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AppUserId",
+                table: "MusicClubs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "MusicClubs");
+        }
+    }
+}
