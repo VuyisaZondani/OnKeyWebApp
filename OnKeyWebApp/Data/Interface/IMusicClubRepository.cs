12e63c6c677d1903b32b4006fd785c2f4b34e8f5
VuyisaZondani <shasazondani@gmail.com>
2023-09-24 10:36:42 +0200

    Updated interface.

--- OnKeyWebApp/Data/Interface/IMusicClubRepository.cs
+++ OnKeyWebApp/Data/Interface/IMusicClubRepository.cs
@@ -6,6 +6,7 @@ namespace OnKeyWebApp.Data.Interface
     {
         Task<IEnumerable<MusicClub>> GetAllMusicClubs();
         Task<MusicClub> GetByIdAsync(int id);
+        Task<MusicClub> GetByIdAsyncNoTracking(int id);
         bool Add(MusicClub musicClub);
         bool Update(MusicClub musicClub);
         bool Delete(MusicClub musicClub);
