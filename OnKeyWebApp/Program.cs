c8e84d8834af1803c216fbaa3497469ca3186613
VuyisaZondani <shasazondani@gmail.com>
2023-09-25 08:22:48 +0200

    .

--- OnKeyWebApp/Program.cs
+++ OnKeyWebApp/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IMusicClubRepository, MusicClubRepository>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IPhotoServices, PhotoService>();
 builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 builder.Services.AddMemoryCache();
